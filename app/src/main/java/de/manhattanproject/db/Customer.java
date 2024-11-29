f3d4a1c46fa5ba3cf969047911ee22afb36cceec
Nico Raphael <nr@qfact.de>
2024-11-29 12:27:54 +0100

    Add customer update logic/test

--- app/src/main/java/de/manhattanproject/db/Customer.java
+++ app/src/main/java/de/manhattanproject/db/Customer.java
@@ -12,18 +12,41 @@ public class Customer implements IDatabaseInteraction<de.manhattanproject.model.
 
     @Override
     public void save(de.manhattanproject.model.Customer customer) throws Exception {
-        try (PreparedStatement stmt = this._db.getConnection().prepareStatement("INSERT INTO customer(id, firstName, lastName, birthDate, gender) VALUES(?,?,?,?,?)")) {
+        try (PreparedStatement stmt = this._db.getConnection().prepareStatement("SELECT id, firstName, lastName, birthDate, gender FROM customer WHERE id=? LIMIT 1")) {
             stmt.setBytes(1, UUID.toBytes(customer.getId()));
-            stmt.setString(2, customer.getFirstName());
-            stmt.setString(3, customer.getLastName());
-            stmt.setDate(4, Date.valueOf(customer.getBirthDate()));
-            stmt.setInt(5, (customer.getGender()).ordinal());
-            stmt.executeUpdate();
-            this._db.getConnection().commit();
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) { //Update if customer exists
+                try (PreparedStatement stmtUpdate = this._db.getConnection().prepareStatement("UPDATE customer SET id=?, firstName=?, lastName=?, birthDate=?, gender=? WHERE id=?")) {
+                    stmtUpdate.setBytes(1, UUID.toBytes(customer.getId()));
+                    stmtUpdate.setString(2, customer.getFirstName());
+                    stmtUpdate.setString(3, customer.getLastName());
+                    stmtUpdate.setDate(4, Date.valueOf(customer.getBirthDate()));
+                    stmtUpdate.setInt(5, (customer.getGender()).ordinal());
+                    stmtUpdate.setBytes(6, UUID.toBytes(customer.getId()));
+                    stmtUpdate.executeUpdate();
+                    this._db.getConnection().commit();
+                } catch (Exception e) {
+                    this._db.getConnection().rollback();
+                    throw e;
+                }
+            } else { //Insert if customer doesn't exist
+                try (PreparedStatement stmtInsert = this._db.getConnection().prepareStatement("INSERT INTO customer(id, firstName, lastName, birthDate, gender) VALUES(?,?,?,?,?)")) {
+                    stmtInsert.setBytes(1, UUID.toBytes(customer.getId()));
+                    stmtInsert.setString(2, customer.getFirstName());
+                    stmtInsert.setString(3, customer.getLastName());
+                    stmtInsert.setDate(4, Date.valueOf(customer.getBirthDate()));
+                    stmtInsert.setInt(5, (customer.getGender()).ordinal());
+                    stmtInsert.executeUpdate();
+                    this._db.getConnection().commit();
+                } catch (Exception e) {
+                    this._db.getConnection().rollback();
+                    throw e;
+                }
+            }
         } catch (Exception e) {
             this._db.getConnection().rollback();
             throw e;
-        }
+        }        
     }
 
     //TODO: Dynamic query filter generation depending on class attributes
